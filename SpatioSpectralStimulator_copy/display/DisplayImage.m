3389b6f0958b0f8a7a809377491e4279c948ac4c
Semin <ohsemin1990@gmail.com>
2024-04-09 10:53:05 +0200

    Update the main code

--- SpatioSpectralStimulator_copy/display/DisplayImage.m
+++ SpatioSpectralStimulator_copy/display/DisplayImage.m
@@ -19,42 +19,24 @@ else
     fprintf('No such directory exist: %s \n',testfiledir);
 end
 
+%% Set variables.
+
+
 %% Start here, if error occurs, we automatically close the PTB screen.
 try
     %% Load the image data.
     testImage = imread('Semin.png');
-    imgScalingFactor = 0.3;
-    testImage = imresize(testImage, imgScalingFactor);
-
-    %% Set the image background right.
-    %
-    % Convert the image to double precision to ensure accurate calculations
-    testImage = im2double(testImage);
-
-    % Calculate the grayscale equivalent of the image
-    grayImage = rgb2gray(testImage);
-
-    % Thresholding to find black regions
-    blackMask = grayImage == 0;
-
-    % Create a gray value for substitution (adjust as desired)
-    bgSetting = 0.5; % This represents gray in the range [0,1]
-
-    % Replace black regions with gray in each channel
-    testImageModified = testImage;
-    for cc = 1:size(testImage, 3)
-        substitutedImageChannel = testImageModified(:,:,cc);
-        substitutedImageChannel(blackMask) = bgSetting;
-        testImageModified(:,:,cc) = substitutedImageChannel;
-    end
-
-    % Here update the image with updated background.
-    testImage = testImageModified;
 
     %% Open the PTB screen.
     initialScreenSetting = [0.5 0.5 0.5]';
     [window windowRect] = OpenPlainScreen(initialScreenSetting);
    
+    %% Make image canvas to present.
+    %
+    % Here we generate an image canvas so that we can present thos whole
+    % image as a stimulus.
+    imageCanvas = MakeImageCanvas(testImage);
+    
     %% Make PTB image texture.
     %
     % Here, we make the PTB texture first, then  we will flip the image in the
@@ -65,7 +47,7 @@ try
     % Also, we will choose which location on the screen to present the image.
     ratioHorintalScreen = 0.5;
     ratioVerticalScreen = 0.5;
-    [imageTexture imageWindowRect rng] = MakeImageTexture(testImage, window, windowRect, ...
+    [imageTexture imageWindowRect rng] = MakeImageTexture(imageCanvas, window, windowRect, ...
         'ratioHorintalScreen',ratioHorintalScreen,'ratioVerticalScreen',ratioVerticalScreen,'verbose', false);
     
     %% Flip the PTB texture to display the image on the projector.
