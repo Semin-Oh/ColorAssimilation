b9208a121eec8cd9d5fea8db7f4f12e3d4ab1dee
Semin <ohsemin1990@gmail.com>
2024-07-29 17:08:35 +0200

    Working on it

--- ColorCalibration/TestDisplay.m
+++ ColorCalibration/TestDisplay.m
@@ -1,6 +1,6 @@
 % TestDisplay.
 %
-% This routine checks and shows the basic characteristics of the display. 
+% This routine checks and shows the basic characteristics of the display.
 
 % History:
 %    07/29/24    smo    - Started on it.
@@ -8,15 +8,35 @@
 %% Initialize.
 clear all; close all;
 
+%% Set variables.
+verbose = true;
+
 %% Load the calibration data.
+%
+% Note that it seems the measurements for the gamma was measured in 10-bit,
+% but the spectra was measured in 8-bit system.
 %
 % Load the spectrum data.
 testFiledir = '/Users/semin/Documents/MATLAB/ColorAssimilation/ColorCalibration/rawdata';
-testFilename = 
+testFilenameSpd = fullfile(testFiledir,'spectra.mat');
+spdData = load(testFilenameSpd);
 
 % Load the photometer data.
+testFilenameGammatable = fullfile(testFiledir,'gammatable.mat');
+gammatableData = load(testFilenameGammatable);
 
 %% Gamma curves.
+%
+% Read out the data.
+inputSettings_gammatable = gammatableData.gammatable.inputSettings;
+output_gammatable_left = gammatableData.gammatable.output.left;
+output_gammatable_right = gammatableData.gammatable.output.right;
+output_gammatable_center = gammatableData.gammatable.output.center;
+
+if (verbose)
+    figure; hold on;
+   
+end
 
 %% Chromaticity diagram.
 
