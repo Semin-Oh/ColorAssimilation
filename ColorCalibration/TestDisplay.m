39b8989a80d806c0a9075ba98ca4e448e334f803
Semin <ohsemin1990@gmail.com>
2024-07-29 16:01:16 +0200

    New script to check the display characteristics

---
+++ ColorCalibration/TestDisplay.m
@@ -0,0 +1,21 @@
+% TestDisplay.
+%
+% This routine checks and shows the basic characteristics of the display. 
+
+% History:
+%    07/29/24    smo    - Started on it.
+
+%% Initialize.
+clear all; close all;
+
+%% Load the calibration data.
+%
+% Load the spectrum data.
+
+% Load the photometer data.
+
+%% Gamma curves.
+
+%% Chromaticity diagram.
+
+%% Additivity test.
