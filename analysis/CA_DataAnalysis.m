d72ab41ed54ee1cc8bf0c225114743c1bf741bfb
Semin <ohsemin1990@gmail.com>
2024-10-09 15:19:35 +0200

    In progress writing analysis code

--- analysis/CA_DataAnalysis.m
+++ analysis/CA_DataAnalysis.m
@@ -7,6 +7,7 @@
 
 % History:
 %    09/09/24    smo    - Started on it.
+%    10/09/24    smo    - Now load the experiment data from the Dropbox.
 
 %% Initialize.
 close all; clear;
@@ -19,16 +20,11 @@ sysInfo = GetComputerInfo();
 % Set the file dir differently depending on the computer.
 switch sysInfo.userShortName
     case 'semin'
-        % Office computer.
-        baseFiledir = '~/Documents/MATLAB';
-
-        % SET THE NAME OF THE LINUX COMPUTER HERE.
-    case 'gegenfurtner'
-        % Lap Linux computer.
-        baseFiledir = '/home/gegenfurtner/Desktop/SEMIN';
+        % Semin office computer.
+        baseFiledir = '/Users/semin/Dropbox (Personal)/Giessen/projects';
     otherwise
-        % This is for Semin's laptop.
-        baseFiledir = 'C:\Users\ohsem\Documents\MATLAB';
+        % Semin's laptop.
+        baseFiledir = 'C:\Users\ohsem\Dropbox (Personal)\Giessen\projects';
 end
 
 % Set repository name.
@@ -49,37 +45,63 @@ subjectName = subjectNames{idxSubject};
 % Set the directory.
 dataFiledir = fullfile(testFiledir,subjectName);
 
-% Read out all avaialble data over different primary color.
-dataFilename_red = GetMostRecentFileName(dataFiledir,sprintf('%s_red',subjectName));
-dataFilename_green = GetMostRecentFileName(dataFiledir,sprintf('%s_green',subjectName));
-dataFilename_blue = GetMostRecentFileName(dataFiledir,sprintf('%s_blue',subjectName));
+% Set experimental mode either 'periphery' or 'fovea'.
+expModeOptions = {'periphery','fovea'};
+expMode = expModeOptions{1};
 
-data_red = load(dataFilename_red);
-data_green = load(dataFilename_green);
-data_blue = load(dataFilename_blue);
+% Choose which primary session.
+whichPrimaryOptions = {'red','green','blue'};
+whichPrimary = whichPrimaryOptions{3};
+
+% Read out all avaialble data over different primary color.
+dataFilename = GetMostRecentFileName(dataFiledir,sprintf('%s_%s_%s',subjectName,expMode,whichPrimary));
+rawData = load(dataFilename);
 
 %% Read out some variables.
-nRepeat = data_red.data.expParams.nRepeat;
-nTestImages = data_red.data.expParams.nTestImages;
+nRepeat = rawData.data.expParams.nRepeat;
+nTestImages = rawData.data.expParams.nTestImages;
 
 %% Rearrange the experiment results.
 %
 % Test images were displayed in a random order, so the raw data is
 % sorted in the same random order. Here, we sort out the results.
 
-% Get the index to sort out the results. 
+% Get the index to sort out the results.
 for rr = 1:nRepeat
     for ii = 1:nTestImages
-        idxOrder(ii,rr) = find(data_red.data.expParams.randOrder(:,rr) == ii);
+        idxOrder(ii,rr) = find(rawData.data.expParams.randOrder(:,rr) == ii);
     end
 end
 
 % Sort out the results.
-matchingIntensityColorCorrect_sorted = data_red.data.matchingIntensityColorCorrect(idxOrder);
+matchingIntensityColorCorrect_sorted = rawData.data.matchingIntensityColorCorrect(idxOrder);
 
 % Mean results.
-mean_red = mean(matchingIntensityColorCorrect_sorted,2);
+meanMatchingIntensityColorCorrect = mean(matchingIntensityColorCorrect_sorted,2);
 
 %% Plot the results.
+%
+% X-axis as test images.
+xaxisTestImages = linspace(1,nTestImages,nTestImages);
+
+% Set marker color over different primaries.
+switch whichPrimary
+    case 'red'
+        markerFaceColor = 'r';
+    case 'green'
+        markerFaceColor = 'g';
+    case 'blue'
+        markerFaceColor = 'b';
+end
+
+% Plot happens here.
+figure; hold on;
+plot(xaxisTestImages,matchingIntensityColorCorrect_sorted,'k.');
+plot(xaxisTestImages,meanMatchingIntensityColorCorrect,'o','MarkerFaceColor',markerFaceColor,'MarkerEdgeColor','k');
+xlabel('Test Image');
+ylabel('Matching intensity');
+xlim([1 nTestImages]);
+xticks(xaxisTestImages);
+ylim([0 0.5]);
 
 %% Save out something if you want.
