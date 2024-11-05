50e8e32dc7169b28f5b74711b2cb2cd13b3c9627
Semin <ohsemin1990@gmail.com>
2024-11-05 11:07:49 +0100

    Now running three primaries as one session

--- experiment/CA_RunExperiment.m
+++ experiment/CA_RunExperiment.m
@@ -19,6 +19,7 @@
 %    09/09/24  smo    - Fixation point has been updated to a circle. Also,
 %                       file name changed.
 %    09/12/24  smo    - Routine is working with the gamepad.
+%    11/05/24  smo    - Now running three primaries as one session.
 
 %% Initialize.
 close all; clear;
@@ -74,39 +75,40 @@ end
 expModeOptions = {'periphery','fovea'};
 expMode = expModeOptions{ansExpMode};
 
-% Decide which stripe color to test.
-while 1
-    inputMessageIdxStripeColor = 'Which color of stripes to test [1:red,2:green,3:blue]: ';
-    idxStripeColor = input(inputMessageIdxStripeColor);
-    idxStripeColorOptions = [1 2 3];
+% % Decide which stripe color to test.
+% while 1
+%     inputMessageIdxStripeColor = 'Which color of stripes to test [1:red,2:green,3:blue]: ';
+%     idxStripeColor = input(inputMessageIdxStripeColor);
+%     idxStripeColorOptions = [1 2 3];
+%
+%     if ismember(idxStripeColor, idxStripeColorOptions)
+%         break
+%     end
+%
+%     disp('Choose among the options [1:red,2:green,3:blue]');
+% end
+% stripeColorOptions = {'red','green','blue'};
+% stripeColorToTest = stripeColorOptions{idxStripeColor};
 
-    if ismember(idxStripeColor, idxStripeColorOptions)
-        break
-    end
-
-    disp('Choose among the options [1:red,2:green,3:blue]');
-end
-stripeColorOptions = {'red','green','blue'};
-stripeColorToTest = stripeColorOptions{idxStripeColor};
-
-% Practice trials.
-while 1
-    inputMessagePracticeTrials = 'Practice trials before main experiment? [Y, N]: ';
-    ansPracticeTrials = input(inputMessagePracticeTrials, 's');
-    ansOptions = {'Y' 'N'};
-
-    if ismember(ansPracticeTrials, ansOptions)
-        break
-    end
-
-    disp('Type either Y or N!');
-end
-
-if (strcmp(ansPracticeTrials,'Y'))
-    PRACTICETRIALS = true;
-elseif (strcmp(ansPracticeTrials,'N'))
-    PRACTICETRIALS = false;
-end
+% COMMENTED OUT FOR NOW.
+% % Practice trials.
+% while 1
+%     inputMessagePracticeTrials = 'Practice trials before main experiment? [Y, N]: ';
+%     ansPracticeTrials = input(inputMessagePracticeTrials, 's');
+%     ansOptions = {'Y' 'N'};
+%
+%     if ismember(ansPracticeTrials, ansOptions)
+%         break
+%     end
+%
+%     disp('Type either Y or N!');
+% end
+%
+% if (strcmp(ansPracticeTrials,'Y'))
+%     PRACTICETRIALS = true;
+% elseif (strcmp(ansPracticeTrials,'N'))
+%     PRACTICETRIALS = false;
+% end
 
 %% Starting from here to the end, if error occurs, we automatically close the PTB screen.
 try
@@ -122,178 +124,195 @@ try
     screenParams.screenGap_cm = 2.5;
 
     % Experimental variables.
-    expParams.nRepeat = 10;
+    expParams.nRepeat = 5;
     expParams.postIntervalDelaySec = 2;
     expParams.postColorCorrectDelaySec = 0.1;
     expParams.subjectName = subjectName;
     expParams.expKeyType = 'gamepad';
+    expParams.stripeColorOptions = {'red','green','blue'};
+    nStripeColors = length(expParams.stripeColorOptions);
+    expParams.stripeColorRandOrder = randperm(nStripeColors);
 
     % etc.
     SAVETHERESULTS = true;
 
-    %% Load test images.
+    %% Experiment starts from here.
     %
     % Get the directory where the test images are saved.
     testImageFiledir = fullfile(testFiledir,'image','TestImages');
 
-    % Load the images here. We will load the corresponding test images
-    % according to stripe color input received at the beginning.
-    testImageFilename = GetMostRecentFileName(testImageFiledir,sprintf('TestImages_%s_%s',expMode,stripeColorToTest));
-    images = load(testImageFilename);
+    % We make a loop to test all stripe colors at once. The order of
+    % showing will be randomized and will save the order in expParams.
+    for ss = 1:nStripeColors
 
-    %% Set the randomization order of displaying the test images.
-    %
-    % Get the info of the number of different test images.
-    expParams.nTestImages = size(images.testImage,1);
+        % Decide which color of stripes to test.
+        idxStripeColor = expParams.stripeColorRandOrder(ss);
+        stripeColorToTest = expParams.stripeColorOptions{idxStripeColor};
 
-    % Set the random order of displaying the test images. For now, we will
-    % display the different test images randomly mixed with faces and eggs.
-    %
-    % The array should look like the number of test images x the number of
-    % repeatitions per each test image. For example, if there are 5 test
-    % images and repeat each test image for 10 times, the array should look
-    % like 5x10.
-    for rr = 1:expParams.nRepeat
-        expParams.randOrder(:,rr) = randperm(expParams.nTestImages)';
-    end
+        % Load the images here. We will load the corresponding test images
+        % according to stripe color input received at the beginning.
+        testImageFilename = GetMostRecentFileName(testImageFiledir,sprintf('TestImages_%s_%s',expMode,stripeColorToTest));
+        images = load(testImageFilename);
 
-    % Sanity check.
-    if any(or(size(expParams.randOrder,1) ~= expParams.nTestImages,...
-            size(expParams.randOrder,2) ~= expParams.nRepeat))
-        error('The random order array size does not match!');
-    end
+        %% Set the randomization order of displaying the test images.
+        %
+        % Get the info of the number of different test images.
+        expParams.nTestImages = size(images.testImage,1);
 
-    % Get the random order of initial test images to display. When doing
-    % color matching, we will display either raw (no color correction) or
-    % the maximum corrected image on the right.
-    %
-    % Here, we set 1 for the raw image and 2 for the max corrected image.
-    initialImageRaw = 1;
-    initialImageMax = 2;
-    expParams.idxRandOrderInitial = randi([initialImageRaw initialImageMax], size(expParams.randOrder));
-    expParams.idxRandOrderInitial(expParams.idxRandOrderInitial == initialImageMax) = images.imageParams.nTestPoints;
-
-    %% Open the PTB screen.
-    initialScreenSetting = [0.5 0.5 0.5]';
-    [window windowRect] = OpenPlainScreen(initialScreenSetting);
-
-    %% Practice trials if you want.
-    if (PRACTICETRIALS)
-
-    end
-
-    %% Display the initial screen on the null image.
-    %
-    % Set the initial screen with written instruction.
-    imageSize = size(images.nullImage);
-    messageInitialImage_1stLine = 'Press any button';
-    messageInitialImage_2ndLine = 'To start the experiment';
-    ratioMessageInitialHorz = 0.49;
-    ratioMessageInitialVert = 0.03;
-
-    % Set the font.
-    switch sysInfo.userShortName
-        case 'gegenfurtner'
-            instructionImageFont = 'DejaVuSans';
-        otherwise
-            instructionImageFont = 'Arial';
-    end
-    initialImageBg = ones(size(images.nullImage))*0.5;
-    initialInstructionImage = insertText(initialImageBg,[imageSize(2)*ratioMessageInitialHorz imageSize(1)/2-imageSize(1)*ratioMessageInitialVert; imageSize(2)*ratioMessageInitialHorz imageSize(1)/2+imageSize(1)*ratioMessageInitialVert],...
-        {messageInitialImage_1stLine messageInitialImage_2ndLine},...
-        'fontsize',40,'Font',instructionImageFont,'BoxColor',[1 1 1],'BoxOpacity',0,'TextColor','black','AnchorPoint','LeftCenter');
-
-    % Display an image texture of the initial image.
-    [initialInstructionImageTexture initialInstructionImageWindowRect rng] = MakeImageTexture(initialInstructionImage, window, windowRect,'verbose',false);
-    FlipImageTexture(initialInstructionImageTexture, window, windowRect,'verbose',false);
-
-    % Get the PTB texture info in an array.
-    activeTextures = [];
-    activeTextures(end+1) = initialInstructionImageTexture;
-
-    % Get any key press to proceed.
-    switch (expParams.expKeyType)
-        case 'gamepad'
-            GetJSResp;
-        case 'keyboard'
-            GetKeyPress;
-    end
-    disp('Experiment is going to be started!');
-
-    %% Color matching experiment happens here.
-    %
-    % Display a null image. We will not include the null image texture in
-    % the 'activeTextures' as we will recall it every after test image
-    % display.
-    [nullImageTexture nullImageWindowRect rng] = MakeImageTexture(images.nullImage, window, windowRect, 'verbose', false);
-    FlipImageTexture(nullImageTexture, window, windowRect,'verbose',false);
-
-    % First loop for the number of trials.
-    for rr = 1:expParams.nRepeat
-
-        % Second loop for different test images. We will display them
-        % together in randomized order.
-        for ii = 1:expParams.nTestImages
-            idxImage = expParams.randOrder(ii,rr);
-            idxColorCorrectImage = expParams.idxRandOrderInitial(ii,rr);
-
-            % Color matching happens within this routine.
-            data.matchingIntensityColorCorrect(ii,rr) = GetOneRespColorMatching(images.testImage,idxImage,idxColorCorrectImage,...
-                images.imageParams.intensityColorCorrect,window,windowRect,...
-                'expKeyType',expParams.expKeyType,'postColorCorrectDelaySec',expParams.postColorCorrectDelaySec,...
-                'verbose',true);
-
-            % Display a null image again and pause for a second before
-            % displaying the next test image.
-            [nullImageTexture nullImageWindowRect rng] = MakeImageTexture(images.nullImage, window, windowRect, 'verbose', false);
-            FlipImageTexture(nullImageTexture, window, windowRect,'verbose',false);
-            pause(expParams.postIntervalDelaySec);
-
-            % Show the progress.
-            fprintf('Experiment progress - (%d/%d) \n',rr,expParams.nRepeat);
+        % Set the random order of displaying the test images. For now, we will
+        % display the different test images randomly mixed with faces and eggs.
+        %
+        % The array should look like the number of test images x the number of
+        % repeatitions per each test image. For example, if there are 5 test
+        % images and repeat each test image for 10 times, the array should look
+        % like 5x10.
+        for rr = 1:expParams.nRepeat
+            expParams.randOrder(:,rr) = randperm(expParams.nTestImages)';
         end
+
+        % Sanity check.
+        if any(or(size(expParams.randOrder,1) ~= expParams.nTestImages,...
+                size(expParams.randOrder,2) ~= expParams.nRepeat))
+            error('The random order array size does not match!');
+        end
+
+        % Get the random order of initial test images to display. When doing
+        % color matching, we will display either raw (no color correction) or
+        % the maximum corrected image on the right.
+        %
+        % Here, we set 1 for the raw image and 2 for the max corrected image.
+        initialImageMin = 1;
+        initialImageMax = 2;
+        expParams.idxRandOrderInitial = randi([initialImageMin initialImageMax], size(expParams.randOrder));
+        expParams.idxRandOrderInitial(expParams.idxRandOrderInitial == initialImageMax) = images.imageParams.nTestPoints;
+
+        %% Open the PTB screen.
+        initialScreenSetting = [0.5 0.5 0.5]';
+        [window windowRect] = OpenPlainScreen(initialScreenSetting);
+
+        %% Practice trials if you want.
+        if (PRACTICETRIALS)
+
+        end
+
+        %% Display the initial screen on the null image.
+        %
+        % Set the initial screen with written instruction.
+        imageSize = size(images.nullImage);
+        messageInitialImage_1stLine = 'Press any button';
+        messageInitialImage_2ndLine = 'To start the experiment';
+        messageInitialImage_3rdLine = sprintf('Type (%s)',expMode);
+        messageInitialImage_4thLine = sprintf('Session (%d/%d)',ss,nStripeColors);
+        ratioMessageInitialHorz = 0.49;
+        ratioMessageInitialVert = 0.03;
+
+        % Set the font.
+        switch sysInfo.userShortName
+            case 'gegenfurtner'
+                instructionImageFont = 'DejaVuSans';
+            otherwise
+                instructionImageFont = 'Arial';
+        end
+        initialImageBg = ones(size(images.nullImage))*0.5;
+        initialInstructionImage = insertText(initialImageBg,[imageSize(2)*ratioMessageInitialHorz imageSize(1)/2-imageSize(1)*ratioMessageInitialVert; imageSize(2)*ratioMessageInitialHorz imageSize(1)/2+imageSize(1)*ratioMessageInitialVert],...
+            {messageInitialImage_1stLine messageInitialImage_2ndLine messageInitialImage_3rdLine messageInitialImage_4thLine},...
+            'fontsize',40,'Font',instructionImageFont,'BoxColor',[1 1 1],'BoxOpacity',0,'TextColor','black','AnchorPoint','LeftCenter');
+
+        % Display an image texture of the initial image.
+        [initialInstructionImageTexture initialInstructionImageWindowRect rng] = MakeImageTexture(initialInstructionImage, window, windowRect,'verbose',false);
+        FlipImageTexture(initialInstructionImageTexture, window, windowRect,'verbose',false);
+
+        % Get the PTB texture info in an array.
+        activeTextures = [];
+        activeTextures(end+1) = initialInstructionImageTexture;
+
+        % Get any key press to proceed.
+        switch (expParams.expKeyType)
+            case 'gamepad'
+                GetJSResp;
+            case 'keyboard'
+                GetKeyPress;
+        end
+        % Make a small delay here in case the button is accidently pressed
+        % again which will affect the next trial.
+        pause(1);
+        disp('Experiment is going to be started!');
+
+        %% Color matching experiment happens here.
+        %
+        % Display a null image. We will not include the null image texture in
+        % the 'activeTextures' as we will recall it every after test image
+        % display.
+        [nullImageTexture nullImageWindowRect rng] = MakeImageTexture(images.nullImage, window, windowRect, 'verbose', false);
+        FlipImageTexture(nullImageTexture, window, windowRect,'verbose',false);
+
+        % First loop for the number of trials.
+        for rr = 1:expParams.nRepeat
+
+            % Second loop for different test images. We will display them
+            % together in randomized order.
+            for ii = 1:expParams.nTestImages
+                idxImage = expParams.randOrder(ii,rr);
+                idxColorCorrectImage = expParams.idxRandOrderInitial(ii,rr);
+
+                % Color matching happens within this routine.
+                data.matchingIntensityColorCorrect(ii,rr) = GetOneRespColorMatching(images.testImage,idxImage,idxColorCorrectImage,...
+                    images.imageParams.intensityColorCorrect,window,windowRect,...
+                    'expKeyType',expParams.expKeyType,'postColorCorrectDelaySec',expParams.postColorCorrectDelaySec,...
+                    'verbose',true);
+
+                % Display a null image again and pause for a second before
+                % displaying the next test image.
+                [nullImageTexture nullImageWindowRect rng] = MakeImageTexture(images.nullImage, window, windowRect, 'verbose', false);
+                FlipImageTexture(nullImageTexture, window, windowRect,'verbose',false);
+                pause(expParams.postIntervalDelaySec);
+
+                % Show the progress.
+                fprintf('Experiment progress - (%d/%d) \n',rr,expParams.nRepeat);
+            end
+        end
+
+    catch
+        % If error occurs, close the screen.
+        CloseScreen;
+        tmpE = lasterror;
+
+        % Display the error message.
+        tmpE.message
+        tmpE.stack.name
+        tmpE.stack.line
     end
 
-catch
-    % If error occurs, close the screen.
+    %% Close the PTB screen once the experiment is done.
     CloseScreen;
-    tmpE = lasterror;
 
-    % Display the error message.
-    tmpE.message
-    tmpE.stack.name
-    tmpE.stack.line
-end
+    %% Save the data.
+    if (SAVETHERESULTS)
+        % Save out the data only if we reached the desired number of trials.
+        nTargetTrials = expParams.nTestImages * expParams.nRepeat;
+        nTrialsDone = ii * rr;
+        if (nTrialsDone == nTargetTrials)
+            saveFiledir = fullfile(testFiledir,'data');
 
-%% Close the PTB screen once the experiment is done.
-CloseScreen;
+            % Make folder with subject name if it does not exist.
+            saveFoldername = fullfile(saveFiledir,subjectName);
+            if ~exist(saveFoldername, 'dir')
+                mkdir(saveFoldername);
+                fprintf('Folder has been successfully created: (%s)\n',saveFoldername);
+            end
 
-%% Save the data.
-if (SAVETHERESULTS)
-    % Save out the data only if we reached the desired number of trials.
-    nTargetTrials = expParams.nTestImages * expParams.nRepeat;
-    nTrialsDone = ii * rr;
-    if (nTrialsDone == nTargetTrials)
-        saveFiledir = fullfile(testFiledir,'data');
+            % Save out the image and experiment params in the structure.
+            data.imageParams = images.imageParams;
+            [~, testImageFilename, ~] = fileparts(testImageFilename);
+            data.imageParams.testImageFilename = testImageFilename;
+            data.expParams = expParams;
 
-        % Make folder with subject name if it does not exist.
-        saveFoldername = fullfile(saveFiledir,subjectName);
-        if ~exist(saveFoldername, 'dir')
-            mkdir(saveFoldername);
-            fprintf('Folder has been successfully created: (%s)\n',saveFoldername);
+            % Set the file name and save.
+            dayTimestr = datestr(now,'yyyy-mm-dd_HH-MM-SS');
+            saveFilename = fullfile(saveFoldername,...
+                sprintf('%s_%s_%s_%s',subjectName,expMode,stripeColorToTest,dayTimestr));
+            save(saveFilename,'data');
+            disp('Data has been saved successfully!');
         end
-
-        % Save out the image and experiment params in the structure.
-        data.imageParams = images.imageParams;
-        [~, testImageFilename, ~] = fileparts(testImageFilename);
-        data.imageParams.testImageFilename = testImageFilename;
-        data.expParams = expParams;
-
-        % Set the file name and save.
-        dayTimestr = datestr(now,'yyyy-mm-dd_HH-MM-SS');
-        saveFilename = fullfile(saveFoldername,...
-            sprintf('%s_%s_%s_%s',subjectName,expMode,stripeColorToTest,dayTimestr));
-        save(saveFilename,'data');
-        disp('Data has been saved successfully!');
     end
 end
