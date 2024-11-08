57601dbf2eb0ce17396f41531fd954e03d5933df
Semin <ohsemin1990@gmail.com>
2024-11-08 17:01:22 +0100

    New script to generate some figures

---
+++ etc/GenerateFigures.m
@@ -0,0 +1,24 @@
+% GenerateFigures.
+%
+% This is for making some images that help to visualize the test images
+% that we used for the project.
+
+% History:
+%    11/08/24    smo    - Wrote it.
+
+%% Initiate.
+clear all; close all;
+
+%% Load images.
+
+%% Make image canvas.
+%
+% Generate the test images with different levels of color corrections to
+% see how the color coordinates change over the power of color correction.
+%
+% MakeImageCanvs
+
+%% Plot it.
+
+
+%% Save out the figures. 
