51aa3d595a2f68ad805f00eb17a136f4d02febd2
Semin <ohsemin1990@gmail.com>
2024-12-06 14:47:07 +0100

    First quick draft checking test images

--- experiment/CA_CheckTestImages.m
+++ experiment/CA_CheckTestImages.m
@@ -34,7 +34,7 @@ testImageProfileDir = fullfile(testFiledir,'image','TestImageProfiles');
 expModeOptions = {'periphery','fovea'};
 stripeColorOptions = {'red','green','blue'};
 expMode = 'periphery';
-stripeColor = 'red';
+stripeColor = 'blue';
 
 %% Load test images
 testImageFilename = GetMostRecentFileName(testImageFiledir,sprintf('TestImages_%s_%s',expMode,stripeColor));
@@ -48,9 +48,9 @@ str_dateTestImages = regexp(testImageFilename , pattern, 'match');
 testImageProfilename = GetMostRecentFileName(testImageProfileDir,sprintf('TestImageProfiles_%s_%s',expMode,stripeColor));
 
 % Check if the date matches as the test image file.
-str_dateTestImageProfiles = regexp(testImageProfilename , pattern, 'match');
+dateStr_TestImageProfiles = regexp(testImageProfilename , pattern, 'match');
 
-if ~strcmp(str_dateTestImages,str_dateTestImageProfiles)
+if ~strcmp(str_dateTestImages,dateStr_TestImageProfiles)
     error('Date mismatch between the test image and image profiles!');
 end
 
@@ -58,5 +58,44 @@ end
 imageProfiles = load(testImageProfilename);
 imageProfiles = imageProfiles.testImageProfile;
 
-%%
+%% Read out some values.
+nTestImages = length(images.imageParams.testImageFilenames);
+nTestPoints = images.imageParams.nTestPoints;
 
+%% Check chromaticity and mean luminance over color corrections.
+%
+% Planckian locus.
+load T_xyzJuddVos
+T_XYZ = T_xyzJuddVos;
+T_xy = [T_XYZ(1,:)./sum(T_XYZ); T_XYZ(2,:)./sum(T_XYZ)];
+T_uv = xyTouv(T_xy);
+
+for ii = 1:nTestImages
+    % Get chromaticity of a striped image.
+    uvY_testImageStripe_temp = imageProfiles{ii,1}.uvY_testImageStripe;
+    mean_uvY_testImageStripe_temp = mean(uvY_testImageStripe_temp,2);
+
+    % Make a new figure per test image.
+    figure; hold on;
+    sgtitle(sprintf('Test image %d / lum = %.2f cd/m2',ii,mean_uvY_testImageStripe_temp(3)));
+    
+    for tt = 1:nTestPoints
+        uvY_colorCorrectedImage_temp = imageProfiles{ii,tt}.uvY_colorCorrectedImage;
+        mean_uvY_colorCorrectedImage_temp = mean(uvY_colorCorrectedImage_temp,2);
+
+        % Plot the chromaticity of color corrected image.
+        subplot(4,ceil(nTestPoints/4),tt); hold on;
+        plot(uvY_colorCorrectedImage_temp(1,:),uvY_colorCorrectedImage_temp(2,:),'.');
+
+        % Plot Planckian locus.
+        plot([T_uv(1,1:65) T_uv(1,1)], [T_uv(2,1:65) T_uv(2,1)], 'k-');
+
+        % Figure stuff.
+        xlim([0 0.7]);
+        ylim([0 0.7]);
+        xlabel('CIE u-prime','fontsize',13);
+        ylabel('CIE v-prime','fontsize',13);
+        title(sprintf('c = %.4f',images.imageParams.intensityColorCorrect(tt)));
+        subtitle(sprintf('Lum = %.2f cd/m2',mean_uvY_colorCorrectedImage_temp(3)));
+    end
+end
