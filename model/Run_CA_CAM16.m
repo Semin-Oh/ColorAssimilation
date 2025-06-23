9df32cb99dc10a6e03330d36b1164945ffa5b88a
Semin <ohsemin1990@gmail.com>
2025-06-23 22:11:57 +0200

    Updates from today

--- model/Run_CA_CAM16.m
+++ model/Run_CA_CAM16.m
@@ -13,13 +13,18 @@
 clear; close all;
 
 %% Set display type.
-displayType = 'EIZO';
+displayType = 'sRGB';
 switch displayType
     case 'EIZO'
         M_RGBToXYZ =  [62.1997 22.8684 19.2310;
             28.5133 78.5446 6.9256;
             0.0739 6.3714 99.5962];
         gamma = 2.1904;
+    case 'sRGB'
+        M_RGBToXYZ = [0.4124564, 0.3575761, 0.1804375;
+            0.2126729, 0.7151522, 0.0721750;
+            0.0193339, 0.1191920, 0.9503041]*100;
+        gamma = 2.2;
 end
 XYZw = sum(M_RGBToXYZ,2);
 
@@ -31,7 +36,7 @@ imageFilenameList = imageFilenameList(~startsWith(imageFilenameList,'.'));
 nImages = length(imageFilenameList);
 
 % Choose which image to play with.
-idxImage = 1;
+idxImage = 2;
 
 % Load the image.
 testImagefilename = fullfile(testImagedir,imageFilenameList{idxImage});
@@ -63,6 +68,10 @@ Jp = (1 + 100 * c1) .* J ./ (1 + c1 * J);
 ap = c2 * C .* cos(h);
 bp = c2 * C .* sin(h);
 
+% Save out the original coordiates here to plot.
+ap_raw = ap;
+bp_raw = bp;
+
 % Reshape to image format.
 Jp_img = reshape(Jp, H, W);
 ap_img = reshape(ap, H, W);
@@ -71,8 +80,8 @@ bp_img = reshape(bp, H, W);
 %% Apply color assimilation on a'b' in CAM16-UCS.
 %
 % Set parameters.
-sigma = 13;
-e = 0.2;
+sigma = 12;
+e = 0.7;
 
 % Add Gaussian filter to each a and b axis on CAM16 UCS. The sigma value
 % defines how much pooling happens from the adjacent area.
@@ -108,17 +117,98 @@ RGB_assim = XYZToRGB(XYZ_assim, M_RGBToXYZ, gamma);
 % Reshape back to image.
 image_assim = reshape(RGB_assim', imageSize);
 
+%% Crop the target image part only.
+%
+% Here we will find the pixel locations where the target images are placed.
+% We take the pixel location using the original image.
+switch idxImage
+    case 2
+        targetRGB = [255 0 0];
+    case 5
+        targetRGB = [202 182 129];
+    otherwise
+        targetRGB = [255 255 255];
+end
+idxImageHeight = [];
+idxImageWidth = [];
+for hh = 1:H
+    for ww = 1:W
+        areAllEqual = (image(hh,ww,1)==targetRGB(1)) & (image(hh,ww,2)==targetRGB(2)) & (image(hh,ww,3)==targetRGB(3));
+        if areAllEqual
+            idxImageHeight(end+1) = hh;
+            idxImageWidth(end+1) = ww;
+        end
+    end
+end
+
+% Get the cropped target image.
+croppedImage = uint8(zeros(imageSize));
+for ii = 1:length(idxImageHeight)
+    croppedImage(idxImageHeight(ii),idxImageWidth(ii),1) = targetRGB(1);
+    croppedImage(idxImageHeight(ii),idxImageWidth(ii),2) = targetRGB(2);
+    croppedImage(idxImageHeight(ii),idxImageWidth(ii),3) = targetRGB(3);
+end
+
+% Get the cropped target image after assimilation.
+croppedImage_assim = uint8(zeros(imageSize));
+for ii = 1:length(idxImageHeight)
+    croppedImage_assim(idxImageHeight(ii),idxImageWidth(ii),1) = image_assim(idxImageHeight(ii),idxImageWidth(ii),1);
+    croppedImage_assim(idxImageHeight(ii),idxImageWidth(ii),2) = image_assim(idxImageHeight(ii),idxImageWidth(ii),2);
+    croppedImage_assim(idxImageHeight(ii),idxImageWidth(ii),3) = image_assim(idxImageHeight(ii),idxImageWidth(ii),3);
+
+    foo(1,ii) = image_assim(idxImageHeight(ii),idxImageWidth(ii),1);
+    foo(2,ii) = image_assim(idxImageHeight(ii),idxImageWidth(ii),2);
+    foo(3,ii) = image_assim(idxImageHeight(ii),idxImageWidth(ii),3);
+end
+
 %% Plot it.
 %
 % Original image and model edited image.
 figure;
-subplot(2,2,1); imshow(image); title('Original Illusion');
-subplot(2,2,2); imshow(image_assim); title('CAM16-UCS Assimilation Output');
+subplot(3,2,1);
+imshow(image);
+title('Original Illusion');
+
+subplot(3,2,2);
+imshow(image_assim);
+title('CA-CAM16 Output');
+
+subplot(3,2,3);
+imshow(croppedImage);
+title('Original Target');
+
+subplot(3,2,4);
+imshow(croppedImage_assim);
+title('CA-CAM16 Output (Target)');
 
 % Heat map of amplifying layer.
-subplot(2,2,3:4);
+subplot(3,2,5:6);
 imagesc(W_map);
 axis image off;
 colormap('hot');  % or try 'parula', 'jet', etc.
 colorbar;
-title(sprintf('Assimilation Weight Map (\\sigma = %.2f, exp = %.2f)', sigma, e));
+title(sprintf('Assimilation Weight Map (\\sigma = %.2f, e = %.2f)', sigma, e));
+
+% Plot it on CAM16-UCS a'b' plane.
+figure;
+subplot(1,2,1);
+plot(ap_raw,bp_raw,'k.');
+xlabel('CAM16-UCS a');
+ylabel('CAM16-UCS b');
+xRange = [-3 3];
+yRange = [-3 3];
+xlim(xRange);
+ylim(yRange);
+axis square;
+grid on;
+title('Original image');
+
+subplot(1,2,2);
+plot(ap_assim,bp_assim,'g.');
+xlabel('CAM16-UCS a');
+ylabel('CAM16-UCS b');
+xlim(xRange);
+ylim(yRange);
+axis square;
+grid on;
+title('Processed image');
